fed32b541431440e4a4288548fe5391d2e56dfbe
GiulianoMonti <giulianm@gmail.com>
2021-06-04 18:32:56 -0300

    Using Java Annotation Configuration

---
+++ core/src/main/java/academy/learnprogramming/AppConfig.java
@@ -0,0 +1,20 @@
+package academy.learnprogramming;
+import org.springframework.context.annotation.*;
+
+
+@Configuration
+@ComponentScan(basePackages = "academy.learnprogramming")
+public class AppConfig {
+
+    // == bean methods ==
+    @Bean
+    public NumberGenerator numberGenerator() {
+        return new NumberGeneratorImpl();
+    }
+
+    @Bean
+    public Game game() {
+        return new GameImpl();
+
+    }
+}
