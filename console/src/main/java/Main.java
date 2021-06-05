816bac32e49ee3e0df05f9b405c107c23ebf8de7
GiulianoMonti <giulianm@gmail.com>
2021-06-05 17:22:10 -0300

    Console Module Setup Challenge

--- console/src/main/java/Main.java
+++ console/src/main/java/Main.java
@@ -1,5 +1,6 @@
-package academy.learnprogramming;
-
+import academy.learnprogramming.AppConfig;
+import academy.learnprogramming.MessageGenerator;
+import academy.learnprogramming.NumberGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -26,8 +27,11 @@ public class Main {
         // log generated number
         log.info("number = {}", number);
 
-        // get game bean from context (container)
-        Game game = context.getBean(Game.class);
+        // get message generator bean from context (container)
+
+        MessageGenerator messageGenerator = context.getBean(MessageGenerator.class);
+        log.info("getMainMessage={}",messageGenerator.getMainMessage());
+        log.info("getResultMessage={}",messageGenerator.getResultMessage());
 
         // close context (container)
         context.close();
