509308fe78a80e207aae88738c6fbbf896268220
GiulianoMonti <giulianm@gmail.com>
2021-06-16 12:16:50 -0300

    end section 4

--- core/src/main/java/academy/learnprogramming/config/GameConfig.java
+++ core/src/main/java/academy/learnprogramming/config/GameConfig.java
@@ -2,16 +2,24 @@ package academy.learnprogramming.config;
 
 import academy.learnprogramming.GuessCount;
 import academy.learnprogramming.MaxNumber;
+import academy.learnprogramming.MinNumber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@ComponentScan(basePackages = "academy.learnprogramming")
+
 @PropertySource("classpath:config/game.properties")
+
 public class GameConfig {
 
     // == fields ==
+    @Value("${game.minNumber:5}")
+    private int minNumber;
+
     @Value("${game.maxNumber:20}")
     private int maxNumber;
 
@@ -32,4 +40,9 @@ public class GameConfig {
         return guessCount;
     }
 
+    @Bean
+    @MinNumber
+    public int minNumber() {
+        return minNumber;
+    }
 }
